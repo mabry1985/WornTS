02a6622967b75dc469fd5f0c45dfe9b1f8f3fc0e
Josh Mabry <mabry1985@gmail.com>
2020-11-05 10:09:20 -0800

    skeleton out collection preview

---
+++ src/components/collection-preview/collection-preview.component.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import "./collection-preview.styles.sass";
+import { CollectionItem } from '../../pages/shop/shop.component';
+
+
+export interface CollectionPreviewProps {
+  title: string;
+  items: CollectionItem[];
+}
+
+export const CollectionPreview: React.FC<CollectionPreviewProps> = ({title, items}) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className="preview">
+      {
+        items.map(item => (
+          <div key={item.id}>item.name</div>
+        ))
+      }
+    </div>
+  </div>
+)
