321a6e1a45784b67940d6b931e025f7337512a64
Josh Mabry <mabry1985@gmail.com>
2020-11-05 10:42:44 -0800

    skeleton collection preview component

--- src/components/collection-preview/collection-preview.component.tsx
+++ src/components/collection-preview/collection-preview.component.tsx
@@ -1,23 +1,27 @@
 import * as React from "react";
 
 import "./collection-preview.styles.sass";
-import { CollectionItem } from '../../pages/shop/shop.component';
-
+import { CollectionItem } from "../../pages/shop/shop.component";
 
 export interface CollectionPreviewProps {
   title: string;
   items: CollectionItem[];
 }
 
-export const CollectionPreview: React.FC<CollectionPreviewProps> = ({title, items}) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+export const CollectionPreview: React.FC<CollectionPreviewProps> = ({
+  title,
+  items,
+}) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {
-        items.map(item => (
-          <div key={item.id}>item.name</div>
-        ))
-      }
+      {items
+        .filter((item, i) => i < 4)
+        .map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
     </div>
   </div>
-)
+);
+
+export default CollectionPreview;
--- src/pages/shop/shop.component.tsx
+++ src/pages/shop/shop.component.tsx
@@ -1,42 +1,44 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data';
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 export type CollectionItem = {
   id: number;
   name: string;
   imageUrl: string;
   price: number;
-}
+};
 
 export type Collection = {
   id: number;
   title: string;
   routeName: string;
   items: CollectionItem[];
-}
+};
 
 export interface ShopPageState {
   collections: Collection[];
-
 }
 
-export interface ShopPageProps {
+export interface ShopPageProps {}
 
-}
-
-export default class ShopPage extends Component<ShopPageProps, ShopPageState>{
-  constructor(props: ShopPageProps){
-    super(props)
+export default class ShopPage extends Component<ShopPageProps, ShopPageState> {
+  constructor(props: ShopPageProps) {
+    super(props);
 
     this.state = {
       collections: SHOP_DATA,
-    }
+    };
   }
   render() {
+    const { collections } = this.state;
     return (
-      <div>
-        Shop Page
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
-    )
+    );
   }
 }
