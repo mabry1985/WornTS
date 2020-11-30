aab94f098da83e7b829761ad83862ce04a692b4f
Josh Mabry <mabry1985@gmail.com>
2020-11-30 11:04:50 -0800

    import shopping component into header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -34,13 +35,14 @@ export const Header: React.FC<HeaderProps> = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
 };
 
 const mapStateToProps = (state: any) => ({
-  currentUser: state.user.currentUser
-})
+  currentUser: state.user.currentUser,
+});
 
 export default connect(mapStateToProps)(Header);
